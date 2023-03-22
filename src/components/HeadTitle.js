81560cfd836e14f2ecd2e022621c5fc1f04eb07d
Newbeer <newbeerpongsakorn@gmail.com>
2023-03-22 21:55:20 +0700

    update-test

--- src/components/HeadTitle.js
+++
@@ -1,8 +0,0 @@
-export const HeadTitle = ({text}) => {
-    return (
-        <div className="head-title mt-3">
-            {text}
-            <div className="head-line" />
-        </div>
-    )
-}
